07e49edb74dc024c1a882b98119e2aac1bf01be7
Arun Kumar M <marunkumar0203@gmail.com>
2026-04-26 17:15:12 +0530

    Add H2 in-memory database configuration for testing

--- src/test/java/org/example/securin/SecurinApplicationTests.java
+++ src/test/java/org/example/securin/SecurinApplicationTests.java
@@ -2,12 +2,13 @@ package org.example.securin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SecurinApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
